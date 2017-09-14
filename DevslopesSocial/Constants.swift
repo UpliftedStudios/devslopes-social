3e25689266cd1d1f608a5dee25919bade6920291
Uplifted Studios <raphaelhidalgo3@gmail.com>
2017-09-14 15:29:24 -0700

    Sign in UI custom classes complete

---
+++ DevslopesSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevslopesSocial
+//
+//  Created by Raphael M. Hidalgo on 9/14/17.
+//  Copyright © 2017 UpliftedStudios. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0

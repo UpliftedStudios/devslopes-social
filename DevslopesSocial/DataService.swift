bc8009f5ad97a22c308245eecdb81dff851a14e1
Uplifted Studios <raphaelhidalgo3@gmail.com>
2017-09-26 15:51:43 -0700

    Completed getting Firebase data

--- DevslopesSocial/DataService.swift
+++ DevslopesSocial/DataService.swift
@@ -32,7 +32,6 @@ class DataService {
     }
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
-        
         REF_USERS.child(uid).updateChildValues(userData)
     }
 }
